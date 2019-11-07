bff7a87507b783eefa2002a24fdb1110d08c1993
marco antonio <marcoantoniozxcv@gmail.com>
2019-11-07 21:47:18 -0200

    banner

--- src/test/java/com/example/Iclean/resources/AnuncioResourceTest.java
+++ src/test/java/com/example/Iclean/resources/AnuncioResourceTest.java
@@ -2,11 +2,14 @@ package com.example.Iclean.resources;
 
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.example.Iclean.entities.Anuncio;
+
 public class AnuncioResourceTest {
 
 	private MockMvc mockMvc;
-	
 	private final String HASH = "/anuncios";
+	private Anuncio anuncio1;
+	private Anuncio anuncio2;
 	
 	
 }
