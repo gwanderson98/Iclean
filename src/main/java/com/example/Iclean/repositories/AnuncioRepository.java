02e0a854fddf3c31b65b24574c21cfb65e1b51e9
Rogerio Siqueira de Miranda <rogercomp@gmail.com>
2019-12-07 17:28:55 -0300

    Testes na entidade Enderecos

--- src/main/java/com/example/Iclean/repositories/AnuncioRepository.java
+++ src/main/java/com/example/Iclean/repositories/AnuncioRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,7 +17,7 @@ public interface AnuncioRepository extends JpaRepository<Anuncio, Long>{
 	Page<Anuncio> findAllByAtivo(Pageable pageable);
 	
 	@Transactional(readOnly = true)
-	@Query("SELECT DISTINCT a FROM Anuncio a WHERE a.titulo LIKE %:palavra%")
+	@Query("SELECT DISTINCT a FROM Anuncio a WHERE LOWER(a.titulo) LIKE %:palavra%")
 	Page<Anuncio> findByTitulo(String palavra, Pageable pageable);
 
 }
--- src/test/java/com/example/Iclean/resources/AnuncioResourceTest.java
+++ src/test/java/com/example/Iclean/resources/AnuncioResourceTest.java
@@ -1,15 +1,90 @@
 package com.example.Iclean.resources;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-import com.example.Iclean.entities.Anuncio;
+import com.example.Iclean.dto.CredentialsDTO;
+import com.example.Iclean.dto.TokenDTO;
+import com.example.Iclean.services.AuthService;
 
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class AnuncioResourceTest {
 
 	private MockMvc mockMvc;
-	private final String HASH = "/anuncios";
-	private Anuncio anuncio1;
-	private Anuncio anuncio2;
+	private String path = "http://localhost:8080/enderecos";
 	
 	
+	@Autowired
+	private WebApplicationContext context;
+	
+	@Autowired
+	private AuthService authService;
+	
+	@Before
+	public void setup() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context).build();
+	}
+	
+	private String obtainAccessToken() throws Exception {
+	 
+		CredentialsDTO cDto = new CredentialsDTO();
+		
+		cDto.setEmail("marcomala@gmail.com");
+		cDto.setSenha("senha321");
+		
+		TokenDTO tokenDto = authService.authenticate(cDto);
+	 
+	    String resultString = tokenDto.getToken();//    // result.andReturn().getResponse().getContentAsString();
+	 
+	    
+	    return resultString;
+	}
+	
+	@Test
+	public void getEnderecoTest200() throws Exception {		
+	    mockMvc.perform(get(path)
+	    		  .param("Authorization", "Bearer " + obtainAccessToken())
+	    	      .contentType(MediaType.APPLICATION_JSON))
+	    	      	.andExpect(status().isOk());
+	    	} 
+	
+	@Test
+	public void getEnderecoTest401() throws Exception {		
+	    mockMvc.perform(get(path + "/3")
+	    	      .contentType(MediaType.APPLICATION_JSON))
+	    	      	.andExpect(status().isUnauthorized());
+	    	}
+	
+	
+	@Test
+	public void getEnderecoTest403() throws Exception {		
+		  mockMvc.perform(get(path + "/2")
+	    		  .param("Authorization", "Bearer " + obtainAccessToken())
+	    	      .contentType(MediaType.APPLICATION_JSON))
+	    	      	.andExpect(status().isForbidden());
+	    	}
+	
+	@Test
+	public void getEnderecoTest404() throws Exception {		
+		  mockMvc.perform(get(path + "/10")
+	    		  .param("Authorization", "Bearer " + obtainAccessToken())
+	    	      .contentType(MediaType.APPLICATION_JSON))
+	    	      	.andExpect(status().isNotFound());
+	    	}
+	
+
 }
